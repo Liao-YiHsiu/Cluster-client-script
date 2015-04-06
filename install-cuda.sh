075f5e4aceaabeaa7a080ad0016d6345860ca648
Yi-Hsiu Liao <nee0224256667@yahoo.com.tw>
2015-04-06 23:27:17 +0800

    override download data

--- install-cuda.sh
+++ install-cuda.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm -f cuda_7.0.28_linux.run
 wget http://developer.download.nvidia.com/compute/cuda/7_0/Prod/local_installers/cuda_7.0.28_linux.run
 chmod +x cuda_7.0.28_linux.run
 init 3
--- install-nvidia.sh
+++ install-nvidia.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm -f NVIDIA-Linux-x86_64-346.47.run
 wget http://tw.download.nvidia.com/XFree86/Linux-x86_64/346.47/NVIDIA-Linux-x86_64-346.47.run
 chmod +x NVIDIA-Linux-x86_64-346.47.run 
 init 3
