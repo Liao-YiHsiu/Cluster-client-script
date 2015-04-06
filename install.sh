736245835ede0f4aa898cb6529921ac87e28818d
Yi-Hsiu Liao <nee0224256667@yahoo.com.tw>
2015-04-07 02:37:44 +0800

    install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
  yum update -y
  yum install -y screen cgdb htop git kernel-devel kernel-headers gcc make
-
- # install cgdb
- which cgdb       2>/dev/null >/dev/null || ./install-cgdb.sh
-
- # install nvidia-driver
- which nvidia-smi 2>/dev/null >/dev/null || ./install-nvidia.sh
-
- # install cuda
- which nvcc       2>/dev/null >/dev/null || ./install-cuda.sh
-
- # install kaldi
- which copy-feats 2>/dev/null >/dev/null || ./install-kaldi.sh
-
- source ~/.bashrc
