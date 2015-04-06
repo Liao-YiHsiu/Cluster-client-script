41aecf9dcce078b3de33223468403c3c366e477e
Yi-Hsiu Liao <nee0224256667@yahoo.com.tw>
2015-04-06 23:22:02 +0800

    bug fixed

--- install-kaldi.sh
+++ install-kaldi.sh
@@ -10,5 +10,5 @@ cd src   ; ./configure && make depend -j 12 && make -j 12 ; cd -;
 now_dir=`pwd`
 
 cp /etc/bashrc tmp
-echo "PATH=$PATH:$now_dir/src/bin:$now_dir/tools/openfst/bin:$now_dir/tools/irstlm/bin/:$now_dir/src/fstbin/:$now_dir/src/gmmbin/:$now_dir/src/featbin/:$now_dir/src/lm/:$now_dir/src/sgmmbin/:$now_dir/src/sgmm2bin/:$now_dir/src/fgmmbin/:$now_dir/src/latbin/:$now_dir/src/nnetbin:$now_dir/src/nnet2bin/:$now_dir/src/kwsbin" > tmp
+echo "PATH=$PATH:$now_dir/src/bin:$now_dir/tools/openfst/bin:$now_dir/tools/irstlm/bin/:$now_dir/src/fstbin/:$now_dir/src/gmmbin/:$now_dir/src/featbin/:$now_dir/src/lm/:$now_dir/src/sgmmbin/:$now_dir/src/sgmm2bin/:$now_dir/src/fgmmbin/:$now_dir/src/latbin/:$now_dir/src/nnetbin:$now_dir/src/nnet2bin/:$now_dir/src/kwsbin" >> tmp
 cp tmp /etc/bashrc
