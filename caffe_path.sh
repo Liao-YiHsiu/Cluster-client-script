6c4516c5d14b68f164c0f88e2fb97298cf65e0a3
Yi-Hsiu Liao <nee0224256667@yahoo.com.tw>
2015-04-29 11:13:18 +0800

    add caffe install script

---
+++ caffe_path.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+CAFFE_PATH=/home_local/speech/Cluster-client-script
+CAFFE=$CAFFE_PATH/caffe
+export PATH=$PATH:$CAFFE/build/tools
+export PYTHONPATH=$CAFFE/python:$PYTHONPATH
---
+++ install-caffe.sh
@@ -0,0 +1,75 @@
+#!/bin/bash -xe
+## Caffe installation on CentOS by simpdanny
+## Require sudo to complete this installation
+YUM_OPTIONS="-y --enablerepo=epel"
+
+## Install location
+CAFFE_PATH=/home_local/speech/Cluster-client-script
+CAFFE=$CAFFE_PATH/caffe
+cd $CAFFE_PATH
+
+## Assume cuda is installed 
+CUDA_PATH=/usr/local/cuda
+
+## Require v2 cudnn for cuda 7.0
+## Require v1 cudnn for cuda 6.5 or below
+CUDNN_PATH=/share/cudnn-6.5-linux-x64-v2
+
+## Install cuDNN
+cp $CUDNN_PATH/cudnn.h $CUDA_PATH/include
+cp $CUDNN_PATH/lib* $CUDA_PATH/lib64
+
+## Expand repository
+## RHEL/CentOS 7 64-Bit ##
+cd /tmp
+wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+rpm -Fivh epel-release-7-5.noarch.rpm
+rm -f /tmp/epel-release-7-5.noarch.rpm
+
+## Install general dependencies
+yum $YUM_OPTIONS install protobuf-devel leveldb-devel snappy-devel opencv-devel boost-devel hdf5-devel
+
+
+## Install more dependencies
+yum $YUM_OPTIONS install gflags-devel glog-devel lmdb-devel
+
+
+
+## Install BLAS
+#yum $YUM_OPTIONS install atlas-devel
+yum $YUM_OPTIONS install openblas-devel.x86_64
+
+## Install Python headers
+yum $YUM_OPTIONS install python-devel
+
+## Require git to clone Caffe on github
+#rm -rf $CAFFE
+cd $CAFFE_PATH
+git clone https://github.com/BVLC/caffe.git
+
+## Config installation by simpdanny's makefile
+CONFIG=/home/simpdanny/Makefile.config
+cp $CONFIG $CAFFE
+cd $CAFFE
+
+## Compile Caffe and run all test
+make all
+make test
+make runtest
+
+## Install python-pip
+yum $YUM_OPTIONS install python-pip
+cd $CAFFE/python
+
+## Install python-wrapper requirements
+for req in $(cat requirements.txt); do pip install $req; done
+pip install numpy
+easy_install -U scikit-image
+
+## install python-wrapper
+cd $CAFFE
+make pycaffe
+
+## export
+export PATH=$PATH:$CAFFE/build/tools
+export PYTHONPATH=$CAFFE/python:$PYTHONPATH
