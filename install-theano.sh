c118e5ac75630ec6b47b26761daab518830a468b
Yi-Hsiu Liao <nee0224256667@yahoo.com.tw>
2015-04-30 14:44:35 +0800

    add theano

---
+++ install-theano.sh
@@ -0,0 +1,5 @@
+yum -y install python-devel python-nose python-setuptools gcc gcc-gfortran gcc-c++ blas-devel lapack-devel atlas-devel
+easy_install pip
+pip install numpy>=1.6.1
+pip install scipy>=0.10.1
+pip install Theano
