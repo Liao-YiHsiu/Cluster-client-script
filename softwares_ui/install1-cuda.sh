c9c22017ae609aa1f7a03a7c14af9cf59d55f945
Yi-Hsiu Liao <nee0224256667@yahoo.com.tw>
2015-09-04 00:41:04 +0800

    bug with cuda install

--- softwares_ui/install1-cuda.sh
+++ softwares_ui/install1-cuda.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
 url="http://developer.download.nvidia.com/compute/cuda/7_0/Prod/local_installers/cuda_7.0.28_linux.run"
-file=$(basename url)
+file=$(basename $url)
 tmp=$(mktemp)
 
 rm -rf $file
