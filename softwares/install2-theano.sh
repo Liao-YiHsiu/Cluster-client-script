7f1afe8535db4b7bdd80d9625bd9935629c0ecb3
Yi-Hsiu Liao <nee0224256667@yahoo.com.tw>
2015-12-28 11:02:30 +0800

    theano update

--- softwares/install2-theano.sh
+++ softwares/install2-theano.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 yum -y install python-devel python-nose python-setuptools gcc gcc-gfortran gcc-c++ blas-devel lapack-devel atlas-devel
 easy_install pip
-pip install numpy>=1.6.1
-pip install scipy>=0.10.1
+pip install --upgrade numpy
+pip install --upgrade scipy
 pip install Theano
