bb2ac65d7e43e6b7566bb5bd918c5fe2561f8c39
Yi-Hsiu Liao <nee0224256667@yahoo.com.tw>
2016-01-23 19:03:44 +0800

    update opencv to yum

--- softwares/install1-opencv.sh
+++ softwares/install1-opencv.sh
@@ -1,17 +1,2 @@
 #!/bin/bash -xe
-
-rm -rf opencv*
-
-wget https://github.com/Itseez/opencv/archive/3.1.0.zip
-unzip 3.1.0.zip
-git clone https://github.com/Itseez/opencv_contrib.git 
-
-cd opencv-3.1.0
-mkdir build
-cd build
-
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
-
-make -j$(nproc)
-make install
-
+yum install -y numpy opencv*
