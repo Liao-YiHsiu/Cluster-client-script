de1dd7aa05a563acdfbc900d960a6f198608de81
Yi-Hsiu Liao <nee0224256667@yahoo.com.tw>
2016-01-23 23:09:33 +0800

    update caffe with speech make

--- softwares/install2-caffe.sh
+++ softwares/install2-caffe.sh
@@ -63,10 +63,11 @@ cd $CAFFE
 #cmake -DBUILD_TIFF=ON -DBLAS=open .. 
 sed -e 's/\(boost_filesystem.*\)$/\0 opencv_core opencv_highgui opencv_imgproc opencv_imgcodecs/' -i Makefile
 export CUDA_VISIBLE_DEVICES=0
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home_local/speech/Cluster-client-script/opencv-3.1.0/build/lib
-make all -j $threads
-make test -j $threads
-make runtest -j $threads
+
+chown speech:speech * -R .
+su -l speech -s /bin/bash -c "cd $CAFFE; make all -j $threads"
+su -l speech -s /bin/bash -c "cd $CAFFE; make test -j $threads"
+su -l speech -s /bin/bash -c "cd $CAFFE; make runtest -j $threads"
 
 ## Install python-pip
 yum $YUM_OPTIONS install python-pip
