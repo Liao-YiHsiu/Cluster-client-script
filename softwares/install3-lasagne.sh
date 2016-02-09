db5653e01b6be3918097cf40c612cf6aba67bfee
Yi-Hsiu Liao <nee0224256667@yahoo.com.tw>
2016-02-09 10:04:23 +0800

    install lasagne stable versison

--- softwares/install3-lasagne.sh
+++ softwares/install3-lasagne.sh
@@ -1,3 +1,2 @@
-git clone https://github.com/Lasagne/Lasagne.git
-cd Lasagne
-pip install --editable .
+pip install -r https://raw.githubusercontent.com/Lasagne/Lasagne/v0.1/requirements.txt
+pip install Lasagne==0.1
