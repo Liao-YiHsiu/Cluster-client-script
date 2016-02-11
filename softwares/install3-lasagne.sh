7e3bd507ecceb4c7f2a8567b06078864442c8079
hoa <r04921047@ntu.edu.tw>
2016-02-11 23:35:38 +0800

    software lasagne update

--- softwares/install3-lasagne.sh
+++ softwares/install3-lasagne.sh
@@ -1,3 +1,2 @@
-git clone https://github.com/Lasagne/Lasagne.git
-cd Lasagne
-pip install --editable .
+pip install -r https://raw.githubusercontent.com/Lasagne/Lasagne/master/requirements.txt
+pip install https://github.com/Lasagne/Lasagne/archive/master.zip
