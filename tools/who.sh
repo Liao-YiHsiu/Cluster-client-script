70238abc6d8d5584785c65209dd69eff8b314776
Yi-Hsiu Liao <nee0224256667@yahoo.com.tw>
2016-04-09 18:35:29 +0800

    add email information

--- tools/who.sh
+++ tools/who.sh
@@ -3,10 +3,11 @@
 if [[ $# -ne 1 ]]; then
    echo "Usage: $0 [user_name] "
    echo 
-   echo "  Lookup the <user_name> in database, return chinese name"
+   echo "  Lookup the <user_name> in database, return chinese name and email."
    exit -1
 fi
 
 user=$1
 
-echo `ldapsearch -x | grep "$user"  -C 1 | grep gecos | cut -d ' ' -f2 | base64 --decode`
+echo "name: " `ldapsearch -x | grep "$user"  -C 1 | grep gecos | cut -d ' ' -f2 | base64 --decode`
+echo `ldapsearch -x | grep "$user"  -C 1 | grep mail 
