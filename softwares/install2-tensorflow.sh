c5bc5383110a1a9525323b9bfcf1d9fae8940006
hoa <r04921047@ntu.edu.tw>
2016-08-10 10:59:16 +0800

    update tensorflow

--- softwares/install2-tensorflow.sh
+++ softwares/install2-tensorflow.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -ex
 
-python2.7 -m pip install https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.5.0-cp27-none-linux_x86_64.whl
+python2.7 -m pip install --upgrade https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.10.0rc0-cp27-none-linux_x86_64.whl
