449205d016b86fb2da309f0934bd399c4914ddb4
hoa <r04921047@ntu.edu.tw>
2016-08-10 11:23:29 +0800

    update tensorflow

--- softwares/install2-tensorflow.sh
+++ softwares/install2-tensorflow.sh
@@ -1,3 +1,8 @@
 #!/bin/bash -ex
 
-python2.7 -m pip install --upgrade https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.10.0rc0-cp27-none-linux_x86_64.whl
+
+url="https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.10.0rc0-cp27-none-linux_x86_64.whl"
+whl=${url##*/}
+wget $url 
+python2.7 -m pip install --upgrade $whl
+rm $whl
