f1f95c119c7ae0cd303946adb27a5b9a421695bc
hoaaoh <b8i3054755@hotmail.com>
2016-10-24 20:42:47 +0800

    tensorflow upgrade

--- softwares/install2-tensorflow.sh
+++ softwares/install2-tensorflow.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -ex
 
-url="https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.10.0rc0-cp27-none-linux_x86_64.whl"
+url="https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.11.0rc1-cp27-none-linux_x86_64.whl"
 whl=${url##*/}
 wget $url 
-#python2.7 -m pip uninstall -y tensorflow protobuf
+#python2.7 -m pip uninstall -y tensorflow protobuf || true
 python2.7 -m pip install --upgrade $whl
 rm $whl
