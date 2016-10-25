d2745786af2c30b908c96ab68673b9394586e112
hoaaoh <b8i3054755@hotmail.com>
2016-10-25 09:20:55 +0800

    tensorflow add python3 version

--- softwares/install2-tensorflow.sh
+++ softwares/install2-tensorflow.sh
@@ -6,3 +6,10 @@ wget $url
 #python2.7 -m pip uninstall -y tensorflow protobuf || true
 python2.7 -m pip install --upgrade $whl
 rm $whl
+
+url="https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.11.0rc1-cp34-cp34m-linux_x86_64.whl"
+whl=${url##*/}
+wget $url
+python3.4 -m pip install --upgrade $whl
+
+rm $whl
