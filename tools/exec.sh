f40d881acdb2cb8fc5410b2278f52f109dc5381f
Yi-Hsiu Liao <nee0224256667@yahoo.com.tw>
2017-02-10 00:48:00 +0800

    use parallel in exec.sh

--- tools/exec.sh
+++ tools/exec.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 curr_dir=`pwd`
+tmp=$(mktemp)
+trap "rm -f $tmp" EXIT
 
 if [ $# -ne 1 ] && [ $# -ne 2 ] ; then
    echo "Usage: $0 [command] <working-dir>"
@@ -18,10 +20,12 @@ fi
 
 hostlist=$(grep 192.168.100.1 /etc/hosts | cut -f2 -d' ' | grep -v Synology)
 
+count=0
 for host in $hostlist;
 do
-   echo "========================================"
-   echo "connecting to $host"
-   ssh -t $host "cd $curr_dir; $1"
-   echo
+   printf "echo '=========>  $host' ;" >> $tmp
+   printf "ssh -t -t $host \"cd $curr_dir; $1\"\n" >> $tmp
+   count=$((count+1))
 done
+
+cat $tmp | parallel  --will-cite  -j $count --work-dir `pwd`
