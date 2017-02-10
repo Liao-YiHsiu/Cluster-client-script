2c7f9961dca3e2a0e0ef9fb3d7bfbb7a12f0ee4f
Yi-Hsiu Liao <nee0224256667@yahoo.com.tw>
2017-02-10 20:44:23 +0800

    add localize.sh to put dir into nfs

---
+++ tools/localize.sh
@@ -0,0 +1,52 @@
+#!/bin/bash -e
+machine=`hostname`
+tmp=$(mktemp)
+trap "rm -f $tmp" EXIT
+
+if [[ $# -gt 2 ]] || [[ $# -eq 0 ]]; then
+   echo "Usage: $0 [<dir> [machine = `hostname`] ]"
+   echo 
+   echo "  put <dir> into /nfs/$machine/$USER/localization "
+   echo "  link this file on all other machines. "
+   exit -1
+fi
+
+path=$(readlink -f $1)
+dirname=$(dirname $path)
+basename=$(basename $path)
+machine=${2:-`hostname`}
+
+# check $machine
+hostlist=$(grep 192.168.100.1 /etc/hosts | cut -f2 -d' ' | grep -v Synology)
+machine=$(echo "$hostlist" | grep -i "\b$machine\b")
+if [ "$machine" == "" ]; then
+   echo "Error! '$2' is not in host list (" $hostlist ")"
+   exit -1
+fi
+
+# check path
+if [ ! -d $path ]; then
+   echo "Error! '$path' is not a directory!"
+   exit -1
+fi
+
+tgt_dir="/nfs/$machine/$USER/localization"
+mkdir -p $tgt_dir
+
+# copy file into local
+echo "copying data from $path to $tgt_dir"
+md5=$(echo $path | md5sum | cut -d ' ' -f 1)
+mv $path $tgt_dir/$md5
+
+# build soft links
+if [ "$(echo $path | grep '^/home/')" != "" ]; then
+   ln -sf $tgt_dir/$md5 $path
+else
+   exec.sh "ln -sf $tgt_dir/$md5 $path"
+fi
+
+# if the original dir is a soft link
+if [ -L $1 ]; then
+   rm $1
+   ln -sf $tgt_dir/$md5 $1
+fi
