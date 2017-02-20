e4467fce19e1b599bab6e7ace3d06bbc5f94ead6
hoaaoh <b8i3054755@hotmail.com>
2017-02-20 16:54:08 +0800

    downgrade

--- softwares/install2-tensorflow.sh
+++ softwares/install2-tensorflow.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -ex
 
 
-url="https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-1.0.0-cp27-none-linux_x86_64.whl"
+url="https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.11.0rc1-cp27-none-linux_x86_64.whl"
 whl=${url##*/}
 wget $url 
 #python2.7 -m pip uninstall -y tensorflow protobuf || true
 python2.7 -m pip install --upgrade $whl
 rm $whl
 
-url="https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-1.0.0-cp34-cp34m-linux_x86_64.whl"
+url="https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.11.0rc1-cp34-cp34m-linux_x86_64.whl"
 whl=${url##*/}
 wget $url
 python3.4 -m pip install --upgrade $whl
