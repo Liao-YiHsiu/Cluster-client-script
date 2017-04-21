68bfbab2ece2bc153e1d712340e4591819d3c3ed
zyi870701 <b99902034@ntu.edu.tw>
2017-04-22 05:15:24 +0800

    add pv to limit bandwidth

--- routine.sh
+++ routine.sh
@@ -80,7 +80,7 @@ find /share_tar/ -iname "*.tgz" -o -iname "*.gz" | while read file; do
    fi
 
    rm -rf $dir/$unzip_file
-   tar zxvf $file -C $dir >/dev/null 2>/dev/null || continue;
+   tar zxvf $file -C $dir | pv -L 1m >/dev/null 2>/dev/null || continue;
 
    find $dir/$unzip_file -type d -exec chmod 755 {} \;
    find $dir/$unzip_file -type f -exec chmod 644 {} \;
