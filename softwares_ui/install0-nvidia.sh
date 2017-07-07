ce6e69cddb81bb6dc3ffc72904db96f5bfccb61d
speech <speech@localhost.localdomain>
2017-07-07 15:16:16 +0800

    add Mazu & update nvidia driver

--- softwares_ui/install0-nvidia.sh
+++ softwares_ui/install0-nvidia.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -ex
 
-url="http://us.download.nvidia.com/XFree86/Linux-x86_64/367.35/NVIDIA-Linux-x86_64-367.35.run"	
+#url="http://us.download.nvidia.com/XFree86/Linux-x86_64/367.35/NVIDIA-Linux-x86_64-367.35.run"	
+url="http://tw.download.nvidia.com/XFree86/Linux-x86_64/381.22/NVIDIA-Linux-x86_64-381.22.run"
 
 file=$(basename $url)
 
